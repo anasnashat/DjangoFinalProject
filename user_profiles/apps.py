83ade29eb65df73b67f870f2eca4f7478089f92f
anas nashat <aanasht@gmail.com>
2025-05-19 18:02:25 +0300

    Add signals to auto-create and update user profiles
    Introduce Django signals to create a `Profile` instance when a `User` is created and update it upon user updates. Ensure the signals are loaded by modifying the app config.

--- user_profiles/apps.py
+++ user_profiles/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UserprofileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_profiles'
+
+    def ready(self):
+        import user_profiles.signals
