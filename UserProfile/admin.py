3a4962331e91dfd922288553603cad89e5261d13
anas nashat <aanasht@gmail.com>
2025-05-19 14:03:05 +0300

    Update Django settings module path in manage.py

--- UserProfile/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile  # Adjust the import path based on your project structure
-
-# Register your models here.
-@admin.register(Profile)
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
