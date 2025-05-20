2c23c6d03b8ab033c8e640ed9ebdf397d842f166
anas nashat <aanasht@gmail.com>
2025-05-20 16:10:41 +0300

    Rename payment session view to donation session in URLs

--- payments/urls.py
+++ payments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create-session/<slug:slug>/', views.create_payment_session, name='create-payment-session')
+    path('create-session/<slug:slug>/', views.create_donation_session, name='create-payment-session'),
     path('success/', views.payment_success, name='payment-success')
 ]
