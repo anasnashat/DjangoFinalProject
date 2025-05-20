e3e24b32299c348c2ed9513ad5345e36ec1380c0
anas nashat <aanasht@gmail.com>
2025-05-20 14:34:50 +0300

    Add widget_tweaks to installed apps

--- donation_project/settings.py
+++ donation_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'widget_tweaks',
     'user_profiles',
     'categories',
     'donations',
