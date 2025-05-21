0e009de59d0b561d3a35dff3eade7103cbf7d7a1
Ali-LGendy <ali.elgondy@gmail.com>
2025-05-21 15:58:55 +0300

    adding users main route

--- donation_project/urls.py
+++ donation_project/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('projects/', include('projects.urls')),
     path('payments/', include('payments.urls')),
     path('donations/', include('donations.urls',namespace='donations')),
+    path('users/', include('users.urls')),
 ]
 
 
